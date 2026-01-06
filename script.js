1d8677fda5acafa0431eaadee8decbebd3842dff
someoneisspecial <insluc65@gmail.com>
2026-01-06 18:33:03 +0530

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 let currentPage = 1;
+const totalPages = 5;
 
 function nextPage() {
-  document.getElementById(`page${currentPage}`).classList.remove("active");
-  currentPage++;
-  document.getElementById(`page${currentPage}`).classList.add("active");
+  if (currentPage < totalPages) {
+    document.getElementById(`page${currentPage}`).classList.remove("active");
+    currentPage++;
+    document.getElementById(`page${currentPage}`).classList.add("active");
+  }
+}
+
+function prevPage() {
+  if (currentPage > 1) {
+    document.getElementById(`page${currentPage}`).classList.remove("active");
+    currentPage--;
+    document.getElementById(`page${currentPage}`).classList.add("active");
+  }
 }
 
 function moveNo() {
   const noBtn = document.querySelector(".no");
   noBtn.style.position = "absolute";
-  noBtn.style.left = Math.random() * 80 + "%";
-  noBtn.style.top = Math.random() * 80 + "%";
+  noBtn.style.left = Math.random() * 70 + "%";
+  noBtn.style.top = Math.random() * 70 + "%";
 }
